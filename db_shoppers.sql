432aee7f12ff1d691ea0bcbe86edd6bd9672bd17
Dale Torre <Dale Torre>
2016-12-19 17:45:38 +0800

    Added shop function.

--- db_shoppers.sql
+++ db_shoppers.sql
@@ -1,28 +1,24 @@
 ﻿# Host: localhost  (Version 5.5.5-10.1.16-MariaDB)
-# Date: 2016-12-19 17:15:58
+# Date: 2016-12-19 17:36:03
 # Generator: MySQL-Front 5.4  (Build 1.40)
 
 /*!40101 SET NAMES utf8 */;
 
 #
-# Structure for table "product"
+# Structure for table "shop"
 #
 
-DROP TABLE IF EXISTS `product`;
-CREATE TABLE `product` (
+DROP TABLE IF EXISTS `shop`;
+CREATE TABLE `shop` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `shopId` int(11) DEFAULT NULL,
-  `baseCategoryId` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `description` text,
-  `price` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `MOP` varchar(20) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `description` varchar(20) DEFAULT NULL,
+  `owner` varchar(20) DEFAULT NULL,
+  `logo` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 #
-# Data for table "product"
+# Data for table "shop"
 #
 
-INSERT INTO `product` VALUES (10,NULL,5,'Small Cabinet','Small Cabinet','1000','1480492814.jpg',NULL),(11,NULL,5,'big cabinet','big','200','1480917918.jpg',NULL),(12,NULL,5,'Medium Cabinet','medium','500','1480931691.jpg',NULL);
---
+++ user/shop/add.php
@@ -0,0 +1,20 @@
+<form action="process.php?action=add" method="POST" enctype="multipart/form-data">
+	<div class="wrapper">
+	<div class="fnwhole">
+	<label>NAME *</label>
+	<input type="text" name="name" placeholder="Add shop">
+	</div>
+	
+	<label>DESCRIPTION *</label>
+	</br>
+	<textarea name="description" placeholder="Input description here" class="wholedescription"></textarea></br>
+	</br>
+	
+	<label>UPLOAD LOGO *</label>
+	</br>
+	<input type="file" name="upload_file"/></br></br>
+	<input type="submit" class="myButton" value="Add">
+	
+
+</form>
+</div>
